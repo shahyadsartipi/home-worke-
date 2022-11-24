f8b44318b9dc0bdd468f11a80616df50b9eaa7bc
shahyad <105771612+shahyadsartipi@users.noreply.github.com>
2022-11-24 18:17:54 +0330

    Update Program.cs

--- p1/ConsoleApp1/Program.cs
+++ p1/ConsoleApp1/Program.cs
@@ -1,14 +1,24 @@
-﻿using System;
+using System;
 class test
 {
-public static void Main(string[] args)
+    public static void Main(string[] args)
+    {
+
+        string  a  = Console.ReadLine();
+        int  x  = Convert.ToInt32(a);
+        Console.WriteLine(tavan_2(x));
+        Console.WriteLine(tavan_3(x));
+    } 
+    private static double tavan_2 (int x)
     {
        
-        int  a = Convert.ToInt32(Console.ReadLine());
-        double x = Math.Pow(a, 2);
-        double y = Math.Pow(x, 3);
-        Console.WriteLine(y);
-        Console.WriteLine(x);
+   
+        double y = Math.Pow(a, 2);
+        double z = Math.Pow(x, 3);
+        return (y);
     }
-
-}
+    private static double tavan_3 (int x)
+    {
+        double y = Math.Pow(a, 3);
+        return(y);
+    }
