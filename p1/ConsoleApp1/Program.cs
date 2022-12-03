20a70c5615d0bef371bc279903c40f14fe61ac53
shahyadsartipi <shahyadsartipi@gmail.com>
2022-12-03 11:50:46 +0330

    edite

--- p1/ConsoleApp1/Program.cs
+++ p1/ConsoleApp1/Program.cs
@@ -4,21 +4,23 @@ class test
     public static void Main(string[] args)
     {
 
-        string  a  = Console.ReadLine();
-        int  x  = Convert.ToInt32(a);
+        string a = Console.ReadLine();
+        int x = Convert.ToInt32(a);
+
         Console.WriteLine(tavan_2(x));
         Console.WriteLine(tavan_3(x));
-    } 
-    private static double tavan_2 (int x)
+    }
+    private static double tavan_2(int x)
     {
-       
-   
-        double y = Math.Pow(a, 2);
+
+
+        double y = Math.Pow(x, 2);
         double z = Math.Pow(x, 3);
         return (y);
     }
-    private static double tavan_3 (int x)
+    private static double tavan_3(int x)
     {
-        double y = Math.Pow(a, 3);
-        return(y);
+        double y = Math.Pow(x, 3);
+        return (y);
     }
+}
